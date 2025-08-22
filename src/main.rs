fecfd4431874fd72b2cfa4efe760ad7d34e0ad4e
FlareFlo <mail@flareflo.dev>
2025-08-22 16:34:57 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use serde::Deserialize;
+use serenity::async_trait;
+use serenity::model::gateway::Ready;
+use serenity::model::guild::Member;
+use serenity::prelude::*;
+use std::fs;
+use std::sync::LazyLock;
+use std::time::Duration;
+
+#[derive(Clone, Deserialize)]
+pub struct Config {
+    pub token: String,
+    pub min_hours: i64,
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn guild_member_addition(&self, ctx: Context, new_member: Member) {
+        let user = &new_member.user;
+        println!("{} just joined!", user.name);
+
+        // Example: check account age
+        let created_at = user.created_at();
+        println!("Their account was created at: {}", created_at);
+
+        // You could kick if too new, e.g. less than 7 days old:
+        let now = chrono::Utc::now();
+        if (now - *created_at).num_hours() < CONFIG.min_hours {
+            if let Err(err) = new_member
+                .kick_with_reason(
+                    &ctx.http,
+                    &format!(
+                        "Your account must be at least {} old",
+                        humantime::format_duration(Duration::from_secs(
+                            (CONFIG.min_hours * 60 * 60) as _
+                        ))
+                    ),
+                )
+                .await
+            {
+                println!("Failed to kick {}: {:?}", user.name, err);
+            } else {
+                println!("Kicked {} for being too new!", user.name);    
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+static CONFIG: LazyLock<Config> = LazyLock::new(|| {
+    toml::from_str::<Config>(&fs::read_to_string("config.toml").unwrap()).unwrap()
+});
+
+#[tokio::main]
+async fn main() {
+    let intents = GatewayIntents::GUILDS | GatewayIntents::GUILD_MEMBERS;
+
+    let mut client = Client::builder(&CONFIG.token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
