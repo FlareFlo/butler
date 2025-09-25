127d64f413d7e1ec0b6ef7e1aa536c0cd7b19d75
FlareFlo <mail@flareflo.dev>
2025-09-25 13:53:40 +0200

    Add message cleanup after kick

--- src/honeypot.rs
+++ src/honeypot.rs
@@ -1,4 +1,7 @@
+use serenity::all::{ChannelType, GetMessages, GuildChannel};
+use serenity::all::Channel;
 use serenity::all::{Context, Message};
+use time::{Duration, UtcDateTime};
 use tracing::{error, warn};
 use crate::{util, CONFIG};
 
@@ -36,6 +39,32 @@ pub async fn handle_honeypot(ctx: Context, msg: &Message) {
                 &reason,
             )
             .await;
+        }
+
+        cleanup_last_hour(&ctx, msg).await;
+    }
+}
+
+/// Deletes all messages of user for past hour
+pub async fn cleanup_last_hour(ctx: &Context, msg: &Message) {
+    let guild_id = msg.guild_id.expect("we know its a guild");
+
+    let user_id = msg.author.id;
+    let one_hour_ago = UtcDateTime::now() - Duration::hours(1);
+
+    // Get all channels in the guild
+    let channels = guild_id.channels(&ctx.http).await.unwrap();
+
+    for (channel_id, channel) in channels {
+        if channel.is_text_based() {
+            // Fetch up to 100 most recent messages (API limit)
+            if let Ok(messages) = channel_id.messages(&ctx.http, GetMessages::new().limit(100)).await {
+                for message in messages {
+                    if message.author.id == user_id && message.timestamp.unix_timestamp() > one_hour_ago.unix_timestamp() {
+                        let _ = channel_id.delete_message(&ctx.http, message.id).await;
+                    }
+                }
+            }
         }
     }
 }
