44f270f91bc33fdf3a017115a4d493e27d12ca42
FlareFlo <mail@flareflo.dev>
2025-10-19 17:00:02 +0200

    Reformat migrations

--- migrations/20251017193733_init.sql
+++ migrations/20251017193733_init.sql
@@ -1,22 +1,25 @@
-CREATE TABLE guilds (
-    id BIGINT PRIMARY KEY,
-    honeypot BIGSERIAL UNIQUE,
+CREATE TABLE guilds
+(
+    id              BIGINT PRIMARY KEY,
+    honeypot        BIGSERIAL UNIQUE,
     logging_channel BIGINT
 );
 
-CREATE TABLE honeypot (
-    id BIGSERIAL PRIMARY KEY REFERENCES guilds(honeypot) ON DELETE CASCADE,
-    channel_ids BIGINT[] NOT NULL,
+CREATE TABLE honeypot
+(
+    id            BIGSERIAL PRIMARY KEY REFERENCES guilds (honeypot) ON DELETE CASCADE,
+    channel_ids   BIGINT[] NOT NULL,
     safe_role_ids BIGINT[] NOT NULL,
-    enabled BOOLEAN NOT NULL
+    enabled       BOOLEAN  NOT NULL
 );
 
 CREATE TYPE moderation_action AS ENUM ('kicked_honeypot', 'kicked_account_age');
 
-CREATE TABLE action_journal (
-    id BIGSERIAL PRIMARY KEY,
-    guild BIGINT NOT NULL, -- No Cascade or Reference, we want to keep journals of offenders
-    offender_id BIGINT NOT NULL,
-    action moderation_action NOT NULL,
-    time timestamptz NOT NULL DEFAULT now()
+CREATE TABLE action_journal
+(
+    id          BIGSERIAL PRIMARY KEY,
+    guild       BIGINT            NOT NULL, -- No Cascade or Reference, we want to keep journals of offenders
+    offender_id BIGINT            NOT NULL,
+    action      moderation_action NOT NULL,
+    time        timestamptz       NOT NULL DEFAULT now()
 );
