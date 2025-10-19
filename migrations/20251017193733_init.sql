839300e0e78949630100e362d5e86c4904b58d45
FlareFlo <mail@flareflo.dev>
2025-10-19 16:59:27 +0200

    Uppercase bigint appropriately

--- migrations/20251017193733_init.sql
+++ migrations/20251017193733_init.sql
@@ -1,13 +1,13 @@
 CREATE TABLE guilds (
-    id bigint PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     honeypot BIGSERIAL UNIQUE,
-    logging_channel bigint
+    logging_channel BIGINT
 );
 
 CREATE TABLE honeypot (
     id BIGSERIAL PRIMARY KEY REFERENCES guilds(honeypot) ON DELETE CASCADE,
-    channel_ids bigint[] NOT NULL,
-    safe_role_ids bigint[] NOT NULL,
+    channel_ids BIGINT[] NOT NULL,
+    safe_role_ids BIGINT[] NOT NULL,
     enabled BOOLEAN NOT NULL
 );
 
@@ -15,8 +15,8 @@ CREATE TYPE moderation_action AS ENUM ('kicked_honeypot', 'kicked_account_age');
 
 CREATE TABLE action_journal (
     id BIGSERIAL PRIMARY KEY,
-    guild bigint NOT NULL, -- No Cascade or Reference, we want to keep journals of offenders
-    offender_id bigint NOT NULL,
+    guild BIGINT NOT NULL, -- No Cascade or Reference, we want to keep journals of offenders
+    offender_id BIGINT NOT NULL,
     action moderation_action NOT NULL,
     time timestamptz NOT NULL DEFAULT now()
 );
