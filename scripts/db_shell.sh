25bb5fab71c381642508b0ac506f924c27b629fc
FlareFlo <mail@flareflo.dev>
2026-01-15 09:26:51 +0100

    Add env var check to db shell

--- scripts/db_shell.sh
+++ scripts/db_shell.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+if [[ -z "$DB_CONTAINER" ]]; then
+    echo "Must provide $DB_CONTAINER in environment" 1>&2
+    exit 1
+fi
 podman exec -it $DB_CONTAINER psql -U botuser botdb
